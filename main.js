ab9860cba0bcaa73c6f347044633ffdd83e5ea2b
y-solb <ysolb99@gmail.com>
2021-03-14 22:29:56 +0900

    JavaScript: event delegation

--- main.js
+++ main.js
@@ -14,29 +14,37 @@ function onAdd() {
   input.value = '';
   input.focus();
 }
-
+let id = 0;
 function createItem(text) {
   const itemRow = document.createElement('li');
   itemRow.setAttribute('class', 'item__row');
+  itemRow.setAttribute('data-id', id);
+  itemRow.innerHTML = `
+    <div class="item">
+      <span class="item__name">${text}</span>
+      <button class="delete__btn">
+        <i class="far fa-trash-alt" data-id=${id}></i>
+      </button>
+    </div>`;
+  id++;
+  // const item = document.createElement('div');
+  // item.setAttribute('class', 'item');
 
-  const item = document.createElement('div');
-  item.setAttribute('class', 'item');
+  // const itemName = document.createElement('span');
+  // itemName.setAttribute('class', 'item__name');
+  // itemName.innerText = text;
 
-  const itemName = document.createElement('span');
-  itemName.setAttribute('class', 'item__name');
-  itemName.innerText = text;
+  // const deleteBtn = document.createElement('button');
+  // deleteBtn.setAttribute('class', 'delete__btn');
+  // deleteBtn.innerHTML = '<i class="far fa-trash-alt"></i>';
+  // deleteBtn.addEventListener('click', () => {
+  //   items.removeChild(itemRow);
+  // });
 
-  const deleteBtn = document.createElement('button');
-  deleteBtn.setAttribute('class', 'delete__btn');
-  deleteBtn.innerHTML = '<i class="far fa-trash-alt"></i>';
-  deleteBtn.addEventListener('click', () => {
-    items.removeChild(itemRow);
-  });
+  // item.appendChild(itemName);
+  // item.appendChild(deleteBtn);
 
-  item.appendChild(itemName);
-  item.appendChild(deleteBtn);
-
-  itemRow.appendChild(item);
+  // itemRow.appendChild(item);
   return itemRow;
 }
 
@@ -47,5 +55,13 @@ addBtn.addEventListener('click', () => {
 input.addEventListener('keypress', (event) => {
   if (event.key === 'Enter') {
     onAdd();
+  }
+});
+
+items.addEventListener('click', () => {
+  const id = event.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
   }
 });
